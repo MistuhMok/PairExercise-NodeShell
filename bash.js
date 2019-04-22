99a71bbe36d48572945b9583597405a3cc1b4d10
Robert S Adams <robert.s.adams32@gmail.com>
2019-04-22 13:08:01 -0400

    updated

--- bash.js
+++ bash.js
@@ -1,10 +1,12 @@
 //output a prompt
 const exportsFromPwd = require('./pwd')
 const ls = require('./ls')
+const cat = require('./cat')
+const curl = require('./curl')
 
 process.stdout.write('prompt > ')
 
-//The stdin 'data' event fires after a suer types in a line
+//The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim(); //remove the newline
 
@@ -17,6 +19,14 @@ process.stdin.on('data', (data) => {
   if (cmd === 'ls') {
     ls(cmd)
   }
+  if (cmd.startsWith('cat ')) {
+    const fileName = cmd.split(' ')[1]
+    cat(fileName)
+  }
 
+  if (cmd.startsWith('curl ')) {
+    const url = cmd.split(' ')[1];
+    curl(url)
+  }
 
 });
